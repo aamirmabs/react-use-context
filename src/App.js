03272cb0edfab506bd274f634fccfd6a451b52ce
aamyear <aamyear@gmail.com>
2021-04-05 10:13:13 +0400

    init of react useContext repo

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React useContext</h1>
+      <p>A detailed introduction...</p>
+    </div>
+  );
+}
+
+export default App;
