ca5414f612bef7631b38a16c3f7fae83d530a0f7
aamyear <aamyear@gmail.com>
2021-04-15 02:45:31 +0400

    useContext with state variables sample code

---
+++ sample-code/04-useContext-with-state-variables/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Store from "./components/Store.js";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React useContext</h1>
+      <p>A detailed introduction...</p>
+      <Store />
+    </div>
+  );
+}
+
+export default App;
